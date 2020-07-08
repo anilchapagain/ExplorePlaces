950401de21db9b3d9d9997c1f9c01fb9729e9d75
anilchapagain <anilchapagain68@gmail.com>
2020-07-08 13:12:37 +0530

    React Context concept use for nave links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -12,9 +12,24 @@ import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import Auth from './user/pages/Auth';
+import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+
+  },
+  []);
+
+
+const logout = useCallback(() => {
+  setIsLoggedIn(false);
+},[]);
+
   return (
+    <AuthContext.Provider value={{isLogedIn: isLoggedIn, login: login, logout:logout}}>
     <Router>
       <MainNavigation />
       <main>
@@ -39,6 +54,7 @@ const App = () => {
         </Switch>
       </main>
     </Router>
+    </AuthContext.Provider>
   );
 };
 
